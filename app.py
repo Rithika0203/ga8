5056490a3c9eb375e4ea475274f946e8f45c50e7
Rithika0203 <119528204+Rithika0203@users.noreply.github.com>
2022-11-30 23:53:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ c=st.number_input("Enter Third Number:")
 
 if (a>b) and (a>c):
 
-  st.write("a is greater")
+  st.write("1st is greater")
 
 elif (b>a) and (b>c):
 
-  st.write("b is greater")
+  st.write("2nd is greater")
 
 else:
 
-  st.write("c is greater")
+  st.write("3rd is greater")
